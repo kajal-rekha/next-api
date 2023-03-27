4a6a9916de8643fe719b8cdd2bc8edc5e760fa2f
kajal-2011 <kajalrekha888@gmail.com>
2023-03-28 02:40:22 +0600

    first commit

---
+++ data/movies.js
@@ -0,0 +1,17 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Spiderman",
+    rating: 8,
+  },
+  {
+    id: 2,
+    title: "Justice League",
+    rating: 8.5,
+  },
+  {
+    id: 3,
+    title: "Shutter Island",
+    rating: 9.1,
+  },
+];
---
+++ pages/movies.js
@@ -0,0 +1,7 @@
+import useSWR from "swr";
+
+const Movies = () => {
+  return <div>Movies</div>;
+};
+
+export default Movies;
