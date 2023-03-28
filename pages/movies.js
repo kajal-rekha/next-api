df0caba0b3b905c6515699ad024c17abde40dcb1
kajal-2011 <kajalrekha888@gmail.com>
2023-03-28 13:26:33 +0600

    done

--- pages/movies.js
+++ pages/movies.js
@@ -1,7 +1,30 @@
 import useSWR from "swr";
 
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) throw new Error("Movies not fetched");
+  return res.json();
+};
 const Movies = () => {
-  return <div>Movies</div>;
+  const { data, error, isValidating } = useSWR("/api/movies", fetcher);
+
+  const isLoading = !data && !error && isValidating;
+
+  if (isLoading) return <h2>Loading...</h2>;
+  if (error) return <h2>{error.message}</h2>;
+
+  return (
+    <div>
+      <h2>Movies</h2>
+      {data.map((movie) => {
+        <div key={movie.id}>
+          <h3>
+            {movie.title} - {movie.rating}
+          </h3>
+        </div>;
+      })}
+    </div>
+  );
 };
 
 export default Movies;
